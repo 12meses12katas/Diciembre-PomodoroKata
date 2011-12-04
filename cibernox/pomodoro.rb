4345b2e82292e45db543f4743dfc39033f6756fc
miguel.camba <miguel.camba@teimas.com>
2011-12-04 18:57:25 +0100

    Ruby version
    I manages start, stop, resume and reset. No GUI

--- cibernox/pomodoro.rb
+++ cibernox/pomodoro.rb
@@ -1,7 +1,7 @@
 class Pomodoro
-  attr_accessor :duration, :remaining_time, :interruptions, :started, :stopped
+  attr_accessor :duration, :remaining_time, :interruptions, :started, :stopped, :timer
   def initialize(time = 25)
-    self.duration = time
+    self.duration = self.remaining_time = time * 60 # Time in minutes
     self.stopped = true
     self.interruptions = 0
   end
@@ -13,12 +13,27 @@ class Pomodoro
   def start
     self.stopped = false
     self.started = true
+    work!
+  end
+
+  def work!
+    self.timer = Thread.new {
+      loop do
+        self.remaining_time -= 5
+        if self.remaining_time <= 0
+          puts "End of pomodoro. Good bye!"
+          exit 
+        end
+        sleep(2) 
+      end
+    }
   end
 
   def stop
     if self.started && !self.stopped # si ha empezado y no está parado
       self.interruptions += 1
       self.stopped = true
+      Thread.kill(self.timer) if self.timer
     else
       false
     end
@@ -26,11 +41,13 @@ class Pomodoro
 
   def resume
     self.stopped = false
+    work!
   end
 
   def reset
     self.remaining_time = duration
     self.interruptions = 0
+    Thread.kill(self.timer) if self.timer
   end
 
   def finished? 
