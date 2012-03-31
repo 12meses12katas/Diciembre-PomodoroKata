819eedcbd2da57be9b0e4bb7cb27fe54cca5bc04
Julius <kujulius@hotmail.com>
2012-03-31 01:11:54 -0400

    Finalizado Ku-Bendezu

--- Julius/pomodoro.rb
+++ Julius/pomodoro.rb
@@ -1,35 +1,47 @@
 class Pomodoro
-	attr_reader :duracion
-	attr_reader :parado
-	attr_reader :interrupciones
-	attr_reader :tiempo_restante
-	attr_reader :temporizador
+ attr_reader :duracion
+ attr_reader :parado
+ attr_reader :interrupciones
+ attr_reader :tiempo_restante
+ attr_reader :temporizador
 
-	def initialize(duracion = 25)
-		@duracion = duracion
-		@parado = true
-		@interrupciones = 0
-		@tiempo_restante = duracion
-	end
+ def initialize(duracion = 25)
+  @duracion = duracion
+  @parado = true
+  @interrupciones = 0
+  @tiempo_restante = duracion * 60 #duracion en minutos y se multiplica por 60 para pasarlo a minutos
+ end
 
-	def interrumpir
-		if not @parado
-			@interrupciones += 1
-		end
-	end
+ def interrumpir
+  if not @parado
+   @interrupciones += 1
+  end
+ end
 
-	def arrancar
-		if @parado
-			@parado = false
-			@temporizador = Thread.new do
-			  loop do
-				  exit if @tiempo_restante <= 0        	
-				  @tiempo_restante -= 1
-				  sleep(1) 
-				  puts @tiempo_restante
-			  end
-	    end
-	  end
-	end
+ def arrancar
+  @interrupciones = 0
+  if @parado
+   @parado = false
+   @temporizador = Thread.new do
+     loop do
+      exit if @tiempo_restante <= 0         
+      @tiempo_restante -= 1
+      sleep(1) 
+      puts @tiempo_restante
+     end
+     end
+   else
+    Thread.kill(@temporizador)
+    @tiempo_restante = @duracion * 60
+    @temporizador = Thread.new do
+     loop do
+      exit if @tiempo_restante <= 0         
+      @tiempo_restante -= 1
+      sleep(1) 
+      #puts @tiempo_restante
+     end 
+     end
+   end
+ end
 
 end
--- Julius/pomodoro_spec.rb
+++ Julius/pomodoro_spec.rb
@@ -2,58 +2,69 @@ require "./pomodoro.rb"
 
 describe Pomodoro do
 
-	before(:each) do
-		@pomodoro = Pomodoro.new
-	end
+ before(:each) do
+  @pomodoro = Pomodoro.new
+ end
 
-	context "CREAR UN POMODORO" do
-		it "un pomodoro dura 25 minutos por defecto" do
-			@pomodoro.duracion.should == 25
-		end
+ context "CREAR UN POMODORO" do
+  it "un pomodoro dura 25 minutos por defecto" do
+   @pomodoro.duracion.should == 25
+  end
 
-		it "un pomodoro puede ser creado con cualquier duracion" do
-	  	@pomodoro = Pomodoro.new(10)
-	  	@pomodoro.duracion.should == 10
-	  end	  	  
-	end
+  it "un pomodoro puede ser creado con cualquier duracion" do
+    @pomodoro = Pomodoro.new(10)
+    @pomodoro.duracion.should == 10
+   end      
+ end
 
-	context "PARAR UN POMODORO" do
-	  it "un pomodoro recien hecho esta parado" do
-	  	@pomodoro.parado.should be_true
-	  end
+ context "PARAR UN POMODORO" do
+   it "un pomodoro recien hecho esta parado" do
+    @pomodoro.parado.should be_true
+   end
 
-	  it "al arrancar un pomodoro comienza la cuenta atras" do
-	  	@pomodoro = Pomodoro.new(10)
-	  	@pomodoro.arrancar
-	  	sleep(5)
-	  	@pomodoro.tiempo_restante.should < 10			
-	  end
+   it "al arrancar un pomodoro comienza la cuenta atras" do
+    @pomodoro = Pomodoro.new(10)
+    @pomodoro.arrancar
+    sleep(5)
+    @pomodoro.tiempo_restante.should < 600  
+   end
 
-	  it "al arrancar un pomodoro comienza la cuenta atras" do
-	  	@pomodoro.tiempo_restante.should > 0
-	  end
+   it "al arrancar un pomodoro comienza la cuenta atras" do
+    @pomodoro.tiempo_restante.should > 0
+   end
 
-	end
+ end
 
-	context "INTERRUMPIR UN POMODORO" do
-		it "un pomodoro se inicia sin interrupciones" do
-	  	@pomodoro.interrupciones.should == 0
-	  end  
+ context "INTERRUMPIR UN POMODORO" do
+  it "un pomodoro se inicia sin interrupciones" do
+    @pomodoro.interrupciones.should == 0
+   end  
 
-	  it "si no esta arrancado no se puede interrumpir" do
-	  	@pomodoro.interrumpir
-	  	@pomodoro.interrupciones.should == 0  	
-	  end
+   it "si no esta arrancado no se puede interrumpir" do
+    @pomodoro.interrumpir
+    @pomodoro.interrupciones.should == 0   
+   end
 
-	  it "el pomodoro cuenta las interrupciones" do
-	  	@pomodoro.arrancar
-	  	@pomodoro.interrumpir
-	  	@pomodoro.interrupciones.should == 1
-	  end
-	end
+   it "el pomodoro cuenta las interrupciones" do
+    @pomodoro.arrancar
+    @pomodoro.interrumpir
+    @pomodoro.interrupciones.should == 1
+   end
+ end
 
-	context "REINICIAR UN POMODORO" do		
-	end
-
-
+ context "REINICIAR UN POMODORO" do
+  it "un pomodoro ya arrancado se reinicia (empieza a contar el tiempo) al arrancarse de nuevo" do
+   @pomodoro = Pomodoro.new(10)
+   @pomodoro.arrancar
+   sleep(5)
+   @pomodoro.arrancar
+   @pomodoro.tiempo_restante.should == 600
+  end
+  it "un pomodoro se reinicia sin interrupciones" do
+   @pomodoro.arrancar
+   @pomodoro.interrumpir
+   @pomodoro.arrancar
+   @pomodoro.interrupciones.should == 0
+  end
+ end
 end
